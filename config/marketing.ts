746aa4c528f45fc3ec7906ac3a97c7c3193d880c
jcarpio <jose.carpio@gmail.com>
2024-10-09 18:58:59 +0200

    Update marketing.ts

--- config/marketing.ts
+++ config/marketing.ts
@@ -3,16 +3,6 @@ import { MarketingConfig } from "types"
 export const marketingConfig: MarketingConfig = {
   mainNav: [
     {
-      title: "Pricing",
-      href: "/pricing",
-    },
-    {
-      title: "Blog",
-      href: "/blog",
-    },
-    {
-      title: "Documentation",
-      href: "/docs",
-    },
+   
   ],
 }
