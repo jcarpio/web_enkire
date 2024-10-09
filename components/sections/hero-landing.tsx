e4056d59bd536e8b20bd13bf658f450ef7a2c9ba
jcarpio <jose.carpio@gmail.com>
2024-10-09 11:59:06 +0200

    Update hero-landing.tsx

--- components/sections/hero-landing.tsx
+++ components/sections/hero-landing.tsx
@@ -30,7 +30,7 @@ export default async function HeroLanding() {
 
         <h1 className="text-balance font-urban text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl lg:text-[66px]">
           <span className="text-gradient_indigo-purple font-extrabold">
-            Enkire
+            Enkire {" "}
           </span>
           technology, love and passion at the {" "}
           <span className="text-gradient_indigo-purple font-extrabold">
@@ -50,36 +50,6 @@ export default async function HeroLanding() {
           className="flex justify-center space-x-2 md:space-x-4"
           style={{ animationDelay: "0.4s", animationFillMode: "forwards" }}
         >
-          <Link
-            href="/pricing"
-            prefetch={true}
-            className={cn(
-              buttonVariants({ size: "lg", rounded: "full" }),
-              "gap-2",
-            )}
-          >
-            <span>Go Pricing</span>
-            <Icons.arrowRight className="size-4" />
-          </Link>
-          <Link
-            href={siteConfig.links.github}
-            target="_blank"
-            rel="noreferrer"
-            className={cn(
-              buttonVariants({
-                variant: "outline",
-                size: "lg",
-                rounded: "full",
-              }),
-              "px-5",
-            )}
-          >
-            <Icons.gitHub className="mr-2 size-4" />
-            <p>
-              <span className="hidden sm:inline-block">Star on</span> GitHub{" "}
-              <span className="font-semibold">{nFormatter(stars)}</span>
-            </p>
-          </Link>
         </div>
       </div>
     </section>
