f557270dc5b3e7a867e5f563ddf04dbfac9e615a
jcarpio <jose.carpio@gmail.com>
2024-10-09 17:05:09 +0200

    Update site-footer.tsx

--- components/layout/site-footer.tsx
+++ components/layout/site-footer.tsx
@@ -40,7 +40,17 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
             Copyright &copy; 2024. All rights reserved.
           </span> */}
           <p className="text-left text-sm text-muted-foreground">
-            Built by{" "}
+             <Link
+              href={"mailto://siteConfig.mailSupport"}
+              target="_blank"
+              rel="noreferrer"
+              className="font-medium underline underline-offset-4"
+            >
+              
+              Contact us{" "}
+            </Link>
+            
+            Open source project started by {" "}
             <Link
               href={siteConfig.links.twitter}
               target="_blank"
