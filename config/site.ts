499aa6dcf69df23a8d00b9975dce5a0e69806d34
jcarpio <jose.carpio@gmail.com>
2024-10-11 12:27:32 +0200

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -6,7 +6,7 @@ const site_url = env.NEXT_PUBLIC_APP_URL;
 export const siteConfig: SiteConfig = {
   name: "Enkire",
   description:
-    "Technology love and passion at the service of the planet.",
+    "Technology love and passion at the service of the world.",
   url: site_url,
   ogImage: `${site_url}/_static/og.jpg`,
   links: {
